9e365d82bd9ae507d0a21f7f376be48fb546467d
Marco Castañeda <marco_pascual410@hotmail.com>
2024-09-22 13:37:21 -0600

    links to stores

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,8 +8,16 @@ use App\Models\User;
 
 class IndexController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
+        $playStoreUrl = "https://play.google.com/store/apps/details?id=com.mtm.icarm";
+        $appStoreUrl = "https://apps.apple.com/mx/app/ayr-morelia/id6449270971?l=en-GB"; 
+        
+        $userAgent = strtolower($request->header('User-Agent'));
 
-        return view('welcome');
+        if (str_contains($userAgent, 'iphone') || str_contains($userAgent, 'ipad') || str_contains($userAgent,'macintosh')) {
+            return redirect($appStoreUrl);
+        } elseif (str_contains($userAgent, 'android') || str_contains($userAgent, 'windows')) {
+            return redirect($playStoreUrl);
+        }
     }
 }
