a06d26c73220c7416bd8a96f73e59b24561f409e
Marco Castañeda <marco_pascual410@hotmail.com>
2024-10-06 13:21:55 -0600

    Update IndexController.php

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -11,7 +11,9 @@ class IndexController extends Controller
     public function index(Request $request){
 
 
-        return view("web.icarm_app.blade.php");
+        //return view("web.icarm_app.blade.php");
+        return view('welcome');
+
 
         $playStoreUrl = "https://play.google.com/store/apps/details?id=com.mtm.icarm";
         $appStoreUrl = "https://apps.apple.com/mx/app/ayr-morelia/id6449270971?l=en-GB"; 
