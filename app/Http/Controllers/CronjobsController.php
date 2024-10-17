8e6e8efd8642aa49732d6a48e527c21962889a21
Marco Castañeda <marco_pascual410@hotmail.com>
2024-10-16 23:08:04 -0600

    Update CronjobsController.php

--- app/Http/Controllers/CronjobsController.php
+++ app/Http/Controllers/CronjobsController.php
@@ -21,9 +21,9 @@ class CronjobsController extends ApiController
     public function reminderEvent(Request $request)
     {
 
-        $eventoSemana = $this->searchEvent(7, 0);    // 7 días antes
+        $eventoSemana = $this->searchEvent(7, 0);    // 7 días antes 
         $eventoDosDias = $this->searchEvent(2, 1);   // 2 días antes
-        $eventoMañana = $this->searchEvent(1, 2);    // 1 día antes
+        $eventoMañana = $this->searchEvent(1, 0);    // 1 día antes
 
         $tokens = FirebaseToken::whereIn('user_id', [2154])
             ->orderBy('created_at')
