fc9635d3ce669ac64ff5fedfec1125810901c402
Marco Castañeda <marco_pascual410@hotmail.com>
2024-10-16 20:58:20 -0600

    Reminder

---
+++ app/Http/Controllers/CronjobsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Evento;
+
+class CronjobsController extends ApiController
+{
+    public $notificationService;
+
+    public function reminderEvent(Request $request)
+    {
+
+        $evento = Evento::where('is_public',1)->where('fecha_inicio', date('Y-m-d', strtotime('+1 day')))->get();
+
+        return $this->ok([
+            'status' => 'Success', 
+            'data' => [
+                'evento'=>$evento,
+            ] 
+        ]);
+    }
+
+}
