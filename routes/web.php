e3955c56e1d4b31ac74c5bf3b2091390bf24914c
Marco Castañeda <marco_pascual410@hotmail.com>
2025-01-28 13:59:57 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'IndexController@index');
 Route::get('/icarm-app', 'IndexController@index');
+Route::get('/radio', 'IndexController@index');
 
 Route::get('/clear', function () {
     Artisan::call('cache:clear');
